49b1fd5a31d639e0dc4352a2347ee1dee37af29d
EnderPluser <95554298+EnderPluser@users.noreply.github.com>
2022-02-06 08:52:25 +0700

    Update APIService.java

--- app/src/main/java/my/insta/androrealm/Messages/Notification/APIService.java
+++ app/src/main/java/my/insta/androrealm/Messages/Notification/APIService.java
@@ -10,7 +10,7 @@ public interface APIService {
     @Headers(
             {
                     "Content-Type:application/json",
-                    "Authorization:key=AAAAAtQdAcU:APA91bEQlb_cftwY8wvTcsDAQgoYk86w1PZP2bAwuOtH0asg_S4ZMDmQu85NsgvBgP7PFIQMNsckt3USVW97U7r-anwa2vNrcrmvw_dgRdO-_nE4I5S_uaZMMkfdBbO6lD7zn_EfZDk-"
+                    "Authorization:key=AAAANc8RTRM:APA91bFQdGgUlBnJU9YAT16dZ05SR8zfqg2wBHvVZypRBcGODLrZoV3aPRHSDRLqS1G3NQ6UxI4qajDxlL-jjO1gW78QIiFj6x5IB6waqP7e2kjvY16o4I6K5T0V5ys1kgF0GCh3bLQJ"
             }
     )
 
